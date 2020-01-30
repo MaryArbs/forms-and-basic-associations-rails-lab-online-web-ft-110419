99367b82e8b3d39ed3b86f0cc14b7e0e4800dbdb
MaryArbs <mary.arbachauskas@gmail.com>
2020-01-30 16:03:22 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
+    
     @song = Song.new(song_params)
 
     if @song.save
@@ -20,6 +21,7 @@ class SongsController < ApplicationController
       render :new
     end
   end
+
 
   def edit
     @song = Song.find(params[:id])
@@ -47,7 +49,9 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
+
+
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+     self.artist.name if self.artist
+  end
+
+  def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(name: genre)
+  end
+
+  def genre_name
+     self.genre.name if self.genre
+  end
+
+  def note_contents
+    self.notes.collect(&:content)
+  end
+
+  def note_contents=(note_contents)
+    note_contents.map do |content|
+      unless content.empty?
+        self.notes << Note.create(content: content)
+        self.save
+      end
+    end
+  end
 end
